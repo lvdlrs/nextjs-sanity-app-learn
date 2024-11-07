c362fd27651f44d7932f2371a4b9c3ff0ebf3a7b
lenmuel <lenmuel.dlrs@gmail.com>
2024-11-07 16:03:20 +0800

    adding hero section schema

--- studio/src/schemaTypes/objects/pagebuilder/heroSection.ts
+++ studio/src/schemaTypes/objects/pagebuilder/heroSection.ts
@@ -33,7 +33,49 @@ export default defineType({
       group: 'content'
     }),
     defineField({
-      
+      name: 'thumbImage',
+      title: 'Thumbnail Image',
+      type: 'image',
+      description: 'This will be use as fallback if background video are not working.',
+      group: 'media'
+    }),
+    defineField({
+      name: 'mediaurl',
+      title: 'Background video',
+      type: 'file',
+      options: {
+        accept: 'video/*'
+      },
+      group: 'media'
+    }),
+    defineField({
+      name: 'overlayColor',
+      title: 'Overlay Color',
+      type: 'string',
+      group: 'styles',
+      initialValue: 'darkoverlay',
+      options: {
+        list: [
+          {title: 'Dark', value: 'darkoverlay'},
+          {title: 'Light', value: 'lightoverlay'},
+          {title: 'No overlay', value: 'nooverlay'},
+        ],
+        layout: 'radio',
+      },
+    }),
+    defineField({
+      name: 'textColor',
+      title: 'Text Color',
+      type: 'string',
+      group: 'styles',
+      initialValue: 'lighttext',
+      options: {
+        list: [
+          {title: 'Dark', value: 'darktext'},
+          {title: 'Light', value: 'lighttext'}
+        ],
+        layout: 'radio',
+      },
     })
   ],
   preview: {
